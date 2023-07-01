73dff0c07e781a4c1fdb3e670f671386d45a4d1a
sagar karotia <sagarkarotia@gmail.com>
2023-07-01 17:22:47 +0530

    shadow

--- src/app/Components/Product.js
+++ src/app/Components/Product.js
@@ -3,12 +3,14 @@ import { Rating } from '@mui/material'
 import React from 'react'
 import { useStateValue } from '../Context/StateProvider'
 import toast, { Toaster } from 'react-hot-toast';
+import Link from 'next/link';
 
 function Product({ image, title, price, rating, description ,id,category}) {
   
   const {product,dispatch} = useStateValue();
   const Array= product.cart;
   // console.log("cart",Array)
+
   const Cart=()=>{
     toast.success('Added to Cart');
     dispatch({
@@ -21,9 +23,9 @@ function Product({ image, title, price, rating, description ,id,category}) {
 
   return (
     <div className='w-full flex justify-center h-full relative'>
-      <div className='w-[22rem]  p-5  flex justify-between flex-col bg-white relative top-2 '> 
+      <div className='w-[22rem]  p-5  flex justify-between flex-col bg-white relative hover:shadow-2xl hover:shadow-black hover:scale-105 duration-500 ease-in-out top-2 '> 
          <h1 className='text-end text-gray-500'>{category}</h1>
-         <img className='w-60 h-60 m-auto object-contain' src={image} alt="" />
+        <Link href='productdetail'> <img className='w-60 h-60 m-auto object-contain' src={image} alt="" /></Link>
         <div>
           <h1 className='font-bold  whitespace-nowrap overflow-hidden text-ellipsis' >{title}</h1>
           <h1 className='line-clamp-2'>{description}</h1>
